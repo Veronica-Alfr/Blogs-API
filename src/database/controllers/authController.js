ab3099c654a88ed3fa6876971a17cd196c4bc17a
Veronica-Alfr <veagalves@gmail.com>
2022-07-15 21:17:43 -0300

    fix: lógica de captura de erro com status 400 com msg correta para cada caso

--- src/database/controllers/authController.js
+++ src/database/controllers/authController.js
@@ -1,11 +1,19 @@
 const authService = require('../services/authService');
 
 const loginUser = async (req, res) => {
-    const { email, password } = authService.validateBody(req.body);
+    try {
+        const { email, password } = authService.validateBody(req.body);
 
-    const token = await authService.login(email, password);
+        const token = await authService.login(email, password);
 
-    res.status(200).json({ token });
+        res.status(200).json({ token });
+    } catch (e) {
+        return res.status(400).json({ message: e.message });
+    }
+
+    // if (!email || !password) {
+    //     return res.status(400).json({ message: 'Some required fields are missing' });
+    // }
 };
 
 module.exports = { loginUser };
